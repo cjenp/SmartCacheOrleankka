f40feee87c52cfd87a109670297223e3a8649424
DESKTOP-5I9EDID\Nejc <pusnik.nejc@gmail.com>
2018-11-09 10:10:35 +0100

    Added a new layer that uses grain casses

---
+++ SmartCacheOrleans/ServiceCode/EmailCheck.cs
@@ -0,0 +1,38 @@
+﻿using ServiceInterface;
+using System;
+using System.Threading.Tasks;
+using Orleans;
+using CacheGrainInter;
+
+namespace ServiceCode
+{
+    public class EmailCheck : IEmailCheck
+    {
+        private IClusterClient client;
+
+        public EmailCheck(IClusterClient c)
+        {
+            client = c;
+        }
+
+        public async Task<bool> AddEmail(string email)
+        {
+            if (email.IndexOf('@') == -1)
+                throw new Exception("Invalid email");
+
+            string domain = email.Substring(email.IndexOf('@'));
+            var grain = client.GetGrain<IDomain>(domain);
+            return await grain.AddEmail(email);
+        }
+
+        public async Task<bool> EmailExists(string email)
+        {
+            if (email.IndexOf('@') == -1)
+                throw new Exception("Invalid email");
+
+            string domain = email.Substring(email.IndexOf('@'));
+            var grain = client.GetGrain<IDomain>(domain);
+            return await grain.Exists(email);
+        }
+    }
+}
---
+++ SmartCacheOrleans/ServiceInterface/IEmailCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ServiceInterface
+{
+    public interface IEmailCheck
+    {
+        /// <summary>
+        /// Check if email exists
+        /// </summary>
+        /// <param name="email">Email address</param>
+        /// <returns>True if email is found, false otherwise</returns>
+        Task<bool> EmailExists(string email);
+
+        /// <summary>
+        /// Adds email
+        /// </summary>
+        /// <param name="email">Email address</param>
+        /// <returns>True if email was added, false if the email allready exists</returns>
+        Task<bool> AddEmail(string email);
+    }
+}
